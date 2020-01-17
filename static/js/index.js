8bc06504af500ba6911d86065bca584e86e03e23
lpjune <laurenporsini@gmail.com>
2020-01-17 15:14:41 -0600

    zooming a video hides the others

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,32 @@
-function zoomin(id){
+function zoomin(id) {
+    var vids = ["vid1", "vid2", "vid3", "vid4", "vid5", "vid6"];
     var myImg = document.getElementById(id);
     var currWidth = myImg.clientWidth;
-    if(currWidth == 200){
-        myImg.style.width =  "500px";
-    } else if(currWidth == 500){
-        myImg.style.width = "200px";
+    if (currWidth == 200) {
+        myImg.style.width = "500px";
+        hideOtherVideos(id);
+    } else if (currWidth == 500) {
+        showAllVideos();
+        for (var i = 0; i < vids.length; i++) {
+            document.getElementById(vids[i]).style = "width:200px";
+        }
     } else {
-        return
+        return;
+    }
+}
+
+function hideOtherVideos(id) {
+    var vids = ["vid1", "vid2", "vid3", "vid4", "vid5", "vid6"];
+    for (var i = 0; i < vids.length; i++) {
+        if (vids[i] !== id) {
+            document.getElementById(vids[i]).style = "display:none";
+        }
+    }
+}
+
+function showAllVideos() {
+    var vids = ["vid1", "vid2", "vid3", "vid4", "vid5", "vid6"];
+    for (var i = 0; i < vids.length; i++) {
+        document.getElementById(vids[i]).style = "display:block";
     }
 }
